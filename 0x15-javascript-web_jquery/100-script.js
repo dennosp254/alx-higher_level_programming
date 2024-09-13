74b90914a242e2114e9fcaa17ff70b2417fcefc1
Dennosp@254 <dennobasil2@gmail.com>
2024-09-13 19:47:54 +0300

    Upgrades

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {  
+    const header = document.querySelector('header'); // Select the <header> element  
+    header.style.color = '#FF0000'; // Change the text color to red  
+});
